c6f1fe537d46a386da6d5f77fa4b26c459e6b0df
jdogcodey <jonnydawson@hotmail.com>
2025-06-20 12:32:42 +0100

    chore: dont know what wasnt committed

---
+++ src/routes/Root.jsx
@@ -0,0 +1,11 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+export default function Root() {
+    return (
+        <>
+        <Header />
+        <Footer />
+        </>
+    )
+}
