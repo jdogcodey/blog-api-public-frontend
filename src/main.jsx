42d6f0d5134c7ccbae65c0a3fd3a746656b64caf
Jonathan Dawson <jonnydawson@hotmail.com>
2025-07-01 11:40:05 +0100

    fix: rename from loginpage to signuppage

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import './index.css'
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
 import Root from './routes/Root.jsx';
 import ErrorPage from './routes/ErrorPage.jsx';
-import Loginpage from './routes/Loginpage.jsx';
+import Loginpage from './routes/SignupPage.jsx';
 import Homepage, { loader as homepageLoader } from './routes/Homepage.jsx';
 
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
     <Route index element={<Homepage />} loader={homepageLoader} />
-    <Route path="/login" element={<Loginpage />} />
+    <Route path="/signup" element={<Loginpage />} />
   </Route>
 ))
 
